dd493c53f95218023e4c111b3cf9ac0e52baaad2
exoticchild <therealenny1@gmail.com>
2020-12-08 08:27:09 -0600

    SLurper
    upgrade

--- main.js
+++ main.js
@@ -27,6 +27,8 @@ const command = args.shift().toLowerCase();
 
 //memes
 
+var m = 60;
+
 var memeimg = [{files: ["./memeimgs/testmeme.jpg"]}, {files: ["./memeimgs/dmeme1.jpg"]}, {files: ["./memeimgs/dmeme2.jpg"]}, {files: ["./memeimgs/dmeme3.jpg"]}, {files: ["./memeimgs/dmeme4.jpg"]},
 {files: ["./memeimgs/dmeme5.jpg"]}, {files: ["./memeimgs/dmeme6.jpg"]}, {files: ["./memeimgs/dmeme7.jpg"]}, {files: ["./memeimgs/dmeme8.jpg"]}, {files: ["./memeimgs/dmeme9.jpg"]}, {files: ["./memeimgs/dmeme10.jpg"]}, 
 {files: ["./memeimgs/dmeme11.jpg"]}, {files: ["./memeimgs/dmeme12.jpg"]}, {files: ["./memeimgs/dmeme13.jpg"]}, {files: ["./memeimgs/dmeme14.jpg"]}, {files: ["./memeimgs/dmeme15.jpg"]}, 
@@ -34,12 +36,11 @@ var memeimg = [{files: ["./memeimgs/testmeme.jpg"]}, {files: ["./memeimgs/dmeme1
 {files: ["./memeimgs/dmeme21.jpg"]}, {files: ["./memeimgs/dmeme22.jpg"]}, {files: ["./memeimgs/dmeme23.jpg"]}, {files: ["./memeimgs/dmeme24.jpg"]}, {files: ["./memeimgs/dmeme25.jpg"]}, 
 {files: ["./memeimgs/dmeme26.jpg"]}, {files: ["./memeimgs/dmeme27.jpg"]}, {files: ["./memeimgs/dmeme28.jpg"]}, {files: ["./memeimgs/dmeme29.jpg"]}, {files: ["./memeimgs/dmeme30.jpg"]}];
 
-var nums = [Math.floor(Math.random()*30), Math.floor(Math.random()*30), Math.floor(Math.random()*30), Math.floor(Math.random()*30), Math.floor(Math.random()*30), 
-    Math.floor(Math.random()*30), Math.floor(Math.random()*30), Math.floor(Math.random()*30), Math.floor(Math.random()*30), Math.floor(Math.random()*30), 
-    Math.floor(Math.random()*30), Math.floor(Math.random()*30), Math.floor(Math.random()*30), Math.floor(Math.random()*30), Math.floor(Math.random()*30), 
-    Math.floor(Math.random()*30), Math.floor(Math.random()*30), Math.floor(Math.random()*30), Math.floor(Math.random()*30), Math.floor(Math.random()*30), 
-    Math.floor(Math.random()*30), Math.floor(Math.random()*30), Math.floor(Math.random()*30), Math.floor(Math.random()*30), Math.floor(Math.random()*30), 
-    Math.floor(Math.random()*30), Math.floor(Math.random()*30), Math.floor(Math.random()*30), Math.floor(Math.random()*30), Math.floor(Math.random()*30)]
+var nums = [];
+
+for(let i = 0; i < 61; i++){
+    nums.push(Math.floor(Math.random()*m));
+}
 
 //yt commands
 if (command === 'ytchannel'){
